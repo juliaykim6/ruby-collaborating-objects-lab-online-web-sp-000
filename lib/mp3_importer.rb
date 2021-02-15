19ffad7c71f6aba3d38f17e9e35da46e114b9b14
Julia Kim <juliaykim6@gmail.com>
2021-02-15 00:53:23 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,7 +5,7 @@ class MP3Importer
     @path = path
   end
 
-  def files 
-    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") } 
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
 end
