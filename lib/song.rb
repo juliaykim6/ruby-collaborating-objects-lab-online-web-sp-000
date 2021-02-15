ecae56f3cb6b26687051e84c61e485aa3b31dec3
Julia Kim <juliaykim6@gmail.com>
2021-02-15 00:34:40 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+     artist.add_song(self)
+  end
+end
