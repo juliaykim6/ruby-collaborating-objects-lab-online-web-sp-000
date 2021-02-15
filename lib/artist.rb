4c499bd09722e1d20e07b042161dbdd0ad93345a
Julia Kim <juliaykim6@gmail.com>
2021-02-15 00:57:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -30,11 +30,11 @@ class Artist
 
   def print_songs
     songs.each {|song| puts song.name}
-  end 
+  end
 
   def add_song_by_title(title)
     song = Song.new(title)
-    song.artist = self 
+    song.artist = self
   end   
 
 end
